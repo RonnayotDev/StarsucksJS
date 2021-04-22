7e2280c6a20f6655bba666c43138652b9e4df4b0
RonnayotDev <73698261+RonnayotDev@users.noreply.github.com>
2021-04-23 01:21:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeImg(click){
+    document.querySelector('.imgChange').src=click;
+    console.log(changeImg);
+}
+function circlechangeColor(click2){
+   document.querySelector('.circle').style.background = click2;
+}
