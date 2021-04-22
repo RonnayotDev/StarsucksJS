ca0e16682097072e726e521e65e45a1d997f1f5d
RonnayotDev <73698261+RonnayotDev@users.noreply.github.com>
2021-04-23 01:29:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeImg(click){
     document.querySelector('.imgChange').src=click;
-    console.log(changeImg);
+    console.log(click);
 }
 function circlechangeColor(click2){
    document.querySelector('.circle').style.background = click2;
